130b04b2f3eed89e2c400c20d48461e0354a9352
yayab42 <yannis.bounia@outlook.com>
2021-02-24 13:13:33 +0100

    Feat/Migration of all tables + model/controller/seeder for usernames table

---
+++ database/seeders/UsernamesSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsernamesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
