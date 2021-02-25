1f119311262e88102193340cd42d5f406c19ba6f
yayab42 <yannis.bounia@outlook.com>
2021-02-25 15:55:47 +0100

    feat:Product seeders

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -13,6 +14,10 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Product::factory()
+            ->count(20)
+           // ->hasCategory(1)
+            ->create();
+
     }
 }
