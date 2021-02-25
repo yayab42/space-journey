7fdadf24d133cb75baf7f0893d7866fb2c068514
Pierrickmartinet <pierrick.martinet@le-campus-numerique.fr>
2021-02-25 09:42:27 +0100

    fix:Rename Models

--- app/Models/Username.php
+++ app/Models/Username.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class space_journey extends Model
+class Username extends Model
 {
     use HasFactory;
 }
