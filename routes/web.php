8218b1c065acdda2239bc5eafcbcd1098160782b
TarikBed <tarik.bedreddine@le-campus-numerique.fr>
2021-03-01 13:58:04 +0100

    feat: Add Controller & view of the Cart + route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
-
 Route::get('product/{product}',[ProductController::class,'show']);
+Route::get('cart',[CartController::class, 'show']);
+
 
