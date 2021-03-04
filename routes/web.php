3874225ea1da09e1d689a8293bbd3f755314401a
yayab42 <yannis.bounia@outlook.com>
2021-03-04 13:11:57 +0100

    fix:routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//route page d'accueil
+Route::get('/', [HomeController::class, 'index']);
+//Route d'un produit précis
+Route::get('product/{product}',[ProductController::class,'show']);
+//Route pour populer la session du panier
+Route::post('cart',[CartController::class,'store']);
+//Route pour la vue du panier construire le panier grâce à la session
+Route::get('cart' , [CartController::class,'index']);
+//Route pour modifier le panier
+Route::post('cartUpdate',[CartController::class, 'update']);
+//Route pour supprimer le panier
+Route::delete('cart',[CartController::class, 'delete'])
+    ->name('cartDelete');
+Route::post('cart/destroy',[CartController::class, 'destroy']);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
