7fb7ca3220c162fb9e0182e9d6b3e10120c127b2
AlexisCosentino <alexis27_08@hotmail.fr>
2021-03-05 09:49:54 +0100

    Feat:Add orderscontroller, route and view. Method addordertobdd added, means when you validate the cart, orders is created automatically in BDD

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+
 
 class OrderController extends Controller
 {
@@ -12,5 +15,14 @@ class OrderController extends Controller
 
     public function addToBdd(){
 
+        $date = date('Y-m-d H:i:s');
+
+        DB::table('orders')->insert([
+            'order_date' =>  $date,
+            'delivery_date' => $date,
+            'number' =>20202020,
+            'users_id' => 1,
+        ]);
+        return view('dashboard', ['date'=>$date]);
     }
 }
